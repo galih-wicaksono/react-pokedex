b42188b18931e87443af1d5ad5b74cc689b549a4
Galih Wicaksono <galih.wicaksono@softwareseni.com>
2024-07-04 09:28:44 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Burger, AppShell } from "@mantine/core"
+
+
+function App() {
+
+  return (
+    <AppShell>
+      <AppShell.Header>
+        <Burger>opened</Burger>
+      </AppShell.Header>
+    </AppShell>
+  )
+}
+
+export default App
